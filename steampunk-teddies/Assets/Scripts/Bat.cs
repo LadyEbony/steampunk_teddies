d2952c6020cccaf2ea60e23efa847190190a4dbd
LadyEbony <jgarc129@ucr.edu>
2018-11-17 14:37:17 -0800

    Added Bat and Melee.

---
+++ steampunk-teddies/Assets/Scripts/Bat.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour {
+  public enum BatState { Standby, meleePause }
+  [Header("State")]
+  public BatState State = BatState.Standby;
+  public float StateDuration = 0.0f;
+
+  [Header("Bat Stats")]
+  public int Damage;
+  public float Speed;
+
+  [Header("Melee Gameobject")]
+  public GameObject Melee;
+  public Transform MeleeTransform;
+
+	void Update () {
+		
+	}
+
+  public void UpdateProcedure() {
+    switch(State) {
+      case BatState.Standby:
+        break;
+
+      case BatState.meleePause:
+        if (StateDuration >= Speed) {
+          SwitchState(BatState.Standby, Speed);
+        }
+        break;
+    }
+    StateDuration += Time.deltaTime;
+  }
+
+  public void Attack() {
+    if (State == BatState.Standby) {
+      var temp = Instantiate(Melee, MeleeTransform.position, MeleeTransform.rotation).GetComponent<Bullet>();
+      temp.Damage = Damage;
+
+      SwitchState(BatState.meleePause);
+    }
+  }
+
+  void SwitchState(BatState state) {
+    State = state;
+    StateDuration = 0.0f;
+  }
+
+  void SwitchState(BatState state, float durationDecrease) {
+    State = state;
+    StateDuration -= durationDecrease;
+  }
+}
---
+++ steampunk-teddies/Assets/Scripts/Melee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour {
+  public int Damage;
+
+  public float DestroyTime;
+  public LayerMask EnemyLayerMask;
+  public LayerMask BulletLayerMask;
+
+	// Use this for initialization
+	void Start () {
+    Destroy(gameObject, DestroyTime);
+	}
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    var layer = collision.gameObject.layer;
+    if (IsInLayerMask(layer, EnemyLayerMask)) {
+      // TODO: Do damage
+      collision.gameObject.GetComponent<CharacterManager>().TakeDamage(Damage);
+
+    } else if (IsInLayerMask(layer, BulletLayerMask)) {
+      var temp = collision.gameObject.GetComponent<Bullet>();
+
+      var rotation = Vector3.Reflect(temp.transform.rotation.eulerAngles, transform.rotation.eulerAngles); 
+      Debug.Log(rotation);
+
+      //temp.transform.rotation = Quaternion.Angle()
+    }
+  }
+
+  private bool IsInLayerMask(int layer, LayerMask layerMask) {
+    return layerMask == (layerMask | (1 << layer));
+  }
+
+}
