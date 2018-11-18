215fcdad01ddb24fca59cd6f68e29626abc00d07
bmpgotcha <bmpgotcha@gmail.com>
2018-11-17 21:29:36 -0800

    Made boss spawner

---
+++ steampunk-teddies/Assets/Scripts/BossSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour {
+    public GameObject boss;
+    public bool once = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( once) {
+            return;
+        }
+        Instantiate(boss, transform.position, Quaternion.identity);
+        once = true;
+    }
+}
