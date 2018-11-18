b3d135c89e3de94afe4185acd872245cf8995d7b
LadyEbony <jgarc129@ucr.edu>
2018-11-17 20:25:49 -0800

    Added Factory Copy prefab.

---
+++ steampunk-teddies/Assets/Scripts/FactoryCopy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryCopy : MonoBehaviour {
+
+  public GameObject FactoryBase;  
+  public int FactoryAmount = 20;
+
+  public Sprite[] sprites;
+
+  [ContextMenu("Copy")]
+  public void Copy() {
+    var amount = transform.childCount;
+
+    while(1 < transform.childCount) {
+      DestroyImmediate(transform.GetChild(1).gameObject);
+    }
+
+    for (var i = 1; i < FactoryAmount; i++) {
+      var temp = Instantiate(FactoryBase, new Vector3(10 * i, 0, 0), Quaternion.identity, transform).GetComponent<SpriteRenderer>();
+      temp.sprite = sprites[Random.Range(0, sprites.Length)];
+
+    }
+  }
+	
+}
