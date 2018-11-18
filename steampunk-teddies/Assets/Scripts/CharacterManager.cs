d64c59f19bfa837761b248715fbf47c8f7cacce9
INeedAUniqueUsername <achen115@ucr.edu>
2018-11-17 18:17:19 -0800

    Implement player invincibility frames

--- steampunk-teddies/Assets/Scripts/CharacterManager.cs
+++ steampunk-teddies/Assets/Scripts/CharacterManager.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterManager : MonoBehaviour {
-  public int maxhp;
-  public int currentHealth;
-  public Gun gunInHand;
-	
+	public int maxhp;
+	public int currentHealth;
+	public Gun gunInHand;
+
 	void Start () {
-    StartProcedure();
+		StartProcedure();
 	}
 
-  protected virtual void StartProcedure() {
-    currentHealth = maxhp;
-    gunInHand.equipped = true;
-  }
-
-  public void TakeDamage(int damage) {
-    Debug.LogFormat("Took {0} damage", damage);
-    currentHealth -= damage;
-    if (currentHealth <= 0)
-    {
-      Destroy(gameObject);
-    }
-  }
+	protected virtual void StartProcedure() {
+	    currentHealth = maxhp;
+	    gunInHand.equipped = true;
+	}
+	public virtual void TakeDamage(int damage) {
+		Debug.LogFormat("Took {0} damage", damage);
+	    currentHealth -= damage;
+	    if (currentHealth <= 0)
+	    {
+	    	Destroy(gameObject);
+	    }
+	}
 	
 }
